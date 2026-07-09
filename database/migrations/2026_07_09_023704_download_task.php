8b9fa9285f821688f28b5f39fbe56def63e921d5
rodrigocarreonc <rodrigocarreon21@gmail.com>
2026-07-08 20:38:40 -0600

    Creation migration download task

---
+++ database/migrations/2026_07_09_023704_download_task.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('download_tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('url');
+            $table->enum('status', ['pending', 'processing', 'completed', 'failed'])->default('pending');
+            $table->string('file_url')->nullable();
+            $table->text('error_message')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('download_tasks');
+    }
+};
